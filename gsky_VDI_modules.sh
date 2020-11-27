b1bfdb64b90305b7af0c553d17b712061de04067
Nigel Rees <nre900@vdi-n15.nci.org.au>
2020-11-27 16:55:05 +1100

    modified:   GSKY_01_WMS.ipynb 	modified:   GSKY_02_WCS.ipynb 	modified:   GSKY_03_ClicknShip.ipynb 	modified:   GSKY_04_ipyleaflet.ipynb 	new file:   gsky_VDI_modules.sh 	modified:   output/gsky_getMap.png 	modified:   output/gsky_wcs.nc 	modified:   output/gsky_wcs.tiff

---
+++ gsky_VDI_modules.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+module purge
+module load jupyterlab/2.1.4-py3.7
+module load python-pillow/7.2.0-py3.7 
+module load python-owslib/0.20.0-py3.7 
+module load gdal/3.1.0-py3.7
+module load netcdf4-python/1.5.3-py3.7 
+module load ipywidgets/7.5.1-py3.7
+module load ipyleaflet/0.13.3-py3.7 
+
